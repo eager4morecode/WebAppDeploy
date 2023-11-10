9c42e1ed1c66c7323a1c04680423391e122f7c1d
eager4morecode <115359627+eager4morecode@users.noreply.github.com>
2023-11-09 21:04:28 -0500

    Create variables.tf
    Define variables

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-west-2"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+// Add more variables as needed...
